b2cddfa0499511e0cb6bb6087a5f97d07d8de05d
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-21 15:54:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,13 @@ def speak_to_grandma(you)
   if you == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
 elsif you == 'Hi Nana, how are you?'
-  puts 'HUH?! SPEAK UP, SONNY!'
+   'HUH?! SPEAK UP, SONNY!'
 elsif  you == 'Hi!'
-  puts 'HUH?! SPEAK UP, SONNY!'
+   'HUH?! SPEAK UP, SONNY!'
 elsif you == 'WHAT DID YOU EAT TODAY?'
-  puts "NO, NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
 elsif you == 'WHAT?'
-  puts "NO, NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
 
   end
 
