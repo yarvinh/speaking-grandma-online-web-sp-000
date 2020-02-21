ecec99c1bf00d6274d2bfb650e0eafd09cb56014
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-21 15:43:37 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,5 +1,5 @@
 
-require 'spec_helper'
+require_relative 'spec_helper'
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
