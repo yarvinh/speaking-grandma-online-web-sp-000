b901d179856493cccb9de3c7231900c1d15c39eb
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-21 15:41:37 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,16 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(you)
+  if you == 'I LOVE YOU GRANDMA!'
+  puts  'I LOVE YOU TOO PUMPKIN!'
+  end
+
+end
